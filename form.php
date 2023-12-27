bb76709fe2ce8e8b8eb18f3841f2cff34d7dfeea
janvizinzuvadiya <136233586+janvizinzuvadiya@users.noreply.github.com>
2023-12-28 00:26:52 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,28 @@
+<?php
+echo"
+<form>
+<br><br><br><br><br><br>
+<table class='tt' align='center' bordercolor='black' style=background-image:url('bg.avif') background-position:'center' >
+<tr style='color:white;'>
+<td><lable>Name:</lable></td>
+<td><input type=text placeholder='Enter Name'></td>
+</tr>
+<tr style='color:white;'>
+<td><lable> E-mail:</lable></td>
+<td><input type=email placeholder='Enter email address'></td>
+</tr>
+<tr style='color:white;'>
+<td><lable> City:</lable></td>
+<td><input type=text placeholder='Enter city name'></td>
+</tr>
+<tr style='color:white;'>
+<td><lable> Address:</lable></td>
+<td><input type=text placeholder='Enter address'></td>
+</tr>
+<tr align='center' style='color:white;'>
+<td colspan=2><input type=submit value='submit'></button></td>
+</tr>
+</table>
+</font>
+</form>";
+?>
